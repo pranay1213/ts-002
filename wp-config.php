6debc0cc77a068dca7cff99a391af672ebeafd7c
pranay1213 <101642245+pranay1213@users.noreply.github.com>
2023-02-11 17:29:41 +0530

    db env set

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,52 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'test-dev' );
 
-/** Database username */
-define( 'DB_USER', 'root' );
+define( 'WP_ENVIRONMENT_TYPE', 'development' );
+switch(getenv("WP_ENVIRONMENT_TYPE")) {
 
-/** Database password */
-define( 'DB_PASSWORD', '' );
 
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
-
-/** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+case "development":
+	define( 'DB_NAME', 'test-dev' );
+	/** Database username */
+	define( 'DB_USER', 'root' );
+	/** Database password */
+	define( 'DB_PASSWORD', '' );
+	/** Database hostname */
+	define( 'DB_HOST', 'localhost' );
+	/** Database charset to use in creating database tables. */
+	define( 'DB_CHARSET', 'utf8' );
+	/** The database collate type. Don't change this if in doubt. */
+	define( 'DB_COLLATE', '' );
+	define( 'WP_DEBUG', true );
+	break;
+case "staging":
+	define( 'DB_NAME', 'dfscdnzhrd' );
+	/** Database username */
+	define( 'DB_USER', 'dfscdnzhrd' );
+	/** Database password */
+	define( 'DB_PASSWORD', 'Gnt4YfRvYS' );
+	/** Database hostname */
+	define( 'DB_HOST', 'localhost' );
+	/** Database charset to use in creating database tables. */
+	define( 'DB_CHARSET', 'utf8' );
+	/** The database collate type. Don't change this if in doubt. */
+	define( 'DB_COLLATE', '' );
+	define( 'WP_DEBUG', false );
+	break;
+default:
+		define( 'DB_NAME', 'test-dev' );
+	/** Database username */
+	define( 'DB_USER', 'root' );
+	/** Database password */
+	define( 'DB_PASSWORD', '' );
+	/** Database hostname */
+	define( 'DB_HOST', 'localhost' );
+	/** Database charset to use in creating database tables. */
+	define( 'DB_CHARSET', 'utf8' );
+	/** The database collate type. Don't change this if in doubt. */
+	define( 'DB_COLLATE', '' );
+}
 
 /**#@+
  * Authentication unique keys and salts.
